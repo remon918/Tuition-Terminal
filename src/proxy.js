8d8b1fc660119d2ae7bbfc897ae329be5ad8f574
remon918 <mdremonhossen7778@gmail.com>
2026-05-22 09:01:38 +0600

    done

--- src/proxy.js
+++ src/proxy.js
@@ -15,5 +15,5 @@ export async function proxy(request) {
 }
  
 export const config = {
-  matcher: ['/tutors/:path', 'add-tutors', 'my-booked-session', 'my-tutors', 'profile']
+  matcher: ['/tutors/:path']
 }
